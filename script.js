16719965803ef47c7b12a36cc4aee935995fecf6
Kristy Jenco <kristyjenco@gmail.com>
2026-05-12 17:58:56 -0400

    Add initial button functionality.

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ const divideNumber = function(firstNumber, secondNumber) {
     return result;
 } 
 let parentContainer = document.getElementById("main");
+let buttons = document.querySelectorAll(".button");
+let operate = function() {
+    buttons.forEach(button => {
+        button.addEventListener("click", event =>{
+            let value = event.target.value;
+        })
+    })
+}
+/*
+1. User presses first button -> button value is stored in variable
+2. User presses second button -> button value is stored in variable
+3. User presses third button -> button value is stored in variable
+4. User presses '=' -> variable values are calculated and stored in textbox
+*/
