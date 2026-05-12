d938bf30cf1dbf6bf897e5f590f856afdc5a5b7f
Kristy Jenco <kristyjenco@gmail.com>
2026-05-12 17:05:01 -0400

    Create initial files.

---
+++ script.js
@@ -0,0 +1,17 @@
+const addNumber = function(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+} 
+const subtractNumber = function(firstNumber, secondNumber) {
+    let result = firstNumber - secondNumber;
+    return result;
+} 
+const multiplyNumber = function(firstNumber, secondNumber) {
+    let result = firstNumber * secondNumber;
+    return result;
+} 
+const divideNumber = function(firstNumber, secondNumber) {
+    let result = firstNumber / secondNumber;
+    return result;
+} 
+let parentContainer = document.getElementById("main");
