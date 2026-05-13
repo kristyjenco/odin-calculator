be815f0dc1641853910012a1cd7b945615c2443e
Kristy Jenco <kristyjenco@gmail.com>
2026-05-13 19:00:43 -0400

    Add functionality to calculate inputs.

--- script.js
+++ script.js
@@ -14,21 +14,43 @@ let parentContainer = document.getElementById("main");
 let textBox = document.getElementById("text");
 let buttons = document.querySelectorAll(".button");
 let currentInput = "";
-let toCalculate = "";
+let firstInput = "";
+let operator = "";
 let operate = function() {
     buttons.forEach(button => {
         button.addEventListener("click", event =>{
             let value = event.target.value;
-            currentInput = currentInput + value;
-            if(value != "+" && value != "-" && value != "*" && value != "/")
+            if(value == "=") {
+                firstInput = Number(firstInput);
+                currentInput = Number(currentInput);
+                if(operator == "+") {
+                   textBox.value = addNumber(firstInput, currentInput);
+                }
+                else if(operator == "-") {
+                    textBox.value = subtractNumber(firstInput, currentInput);
+                }
+                else if(operator == "*") {
+                    textBox.value = multiplyNumber(firstInput, currentInput);
+                }
+                else if(operator == "/") {
+                    textBox.value = divideNumber(firstInput, currentInput);
+                }
+            }
+            else if(value != "+" && value != "-" && value != "*" && value != "/" && value != "=")
             {
+                currentInput = currentInput + value;
                 textBox.value = currentInput;
+            }
+            else if(value == "clear") {
+                currentInput = "";
+                firstInput = "";
+                operator = "";
             }
             else {
                 textBox.value = value;
-                toCalculate = currentInput;
+                firstInput = currentInput;
                 currentInput = "";
-                
+                operator = value;
             }
         })
     })
